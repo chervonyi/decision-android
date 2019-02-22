8a176a7fce8bfac2a30b4c3449f79e09a98f30f8
chervonyi <yuri.ternopil@gmail.com>
2019-02-21 20:15:56 -0600

    Add DisclaimerActivity
    Also remove TitleBar and StatusBar

--- app/src/main/java/chrgames/decision/DisclaimerActivity.java
+++ app/src/main/java/chrgames/decision/DisclaimerActivity.java
@@ -2,6 +2,8 @@ package chrgames.decision;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class DisclaimerActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class DisclaimerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disclaimer);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            Toast.makeText(this, "PRESSED", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onTouchEvent(event);
     }
 }
