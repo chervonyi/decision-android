3e956b7a75b8a885fce5d2d29d1fdaedda257b56
chervonyi <yuri.ternopil@gmail.com>
2019-02-21 21:13:09 -0600

    Add ChapterActivity
    + Fix problem with size of textViews

--- app/src/main/java/chrgames/decision/ChapterActivity.java
+++ app/src/main/java/chrgames/decision/ChapterActivity.java
@@ -1,26 +1,23 @@
 package chrgames.decision;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
-public class DisclaimerActivity extends AppCompatActivity {
+public class ChapterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_disclaimer);
+        setContentView(R.layout.activity_chapter);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
         if (event.getAction() == MotionEvent.ACTION_UP) {
-            //Toast.makeText(this, "PRESSED", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, ChapterActivity.class);
-            startActivity(intent);
+            Toast.makeText(this, "PRESSED", Toast.LENGTH_SHORT).show();
         }
 
         return super.onTouchEvent(event);
